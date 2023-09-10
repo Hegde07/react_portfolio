a138379754f2d88477bde3840100acc080a68cf2
Hegde07 <hegdebhargav92@gmail.com>
2023-09-09 17:20:56 -0700

    Updated Content

--- src/data/constants.js
+++ src/data/constants.js
@@ -18,7 +18,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Hegde07",
   resume:
-    "",
+    "https://drive.google.com/file/d/1E5sQnYJcRi4GbTALa77auYDtwWfJWtI7/view?usp=sharing",
   linkedin: "linkedin.com/in/bhargav-hegde-",
   twitter: "https://twitter.com/BhargavHegde17",
   insta: "",
